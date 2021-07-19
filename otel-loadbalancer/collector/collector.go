f0a309278c111b80230c025ff987a744527d3613
Rahul Varma <rahvarm@amazon.com>
2021-07-19 10:09:26 -0700

    Updated to add a mutex locks and watcher timeout

--- otel-loadbalancer/collector/collector.go
+++ otel-loadbalancer/collector/collector.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"log"
 	"os"
+	"time"
 
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -11,6 +12,14 @@ import (
 	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
+)
+
+const (
+	watcherTimeout = 15 * time.Minute
+)
+
+var (
+	ns = os.Getenv("OTEL_NAMESPACE")
 )
 
 type Client struct {
@@ -36,7 +45,6 @@ func NewClient() (Client, error) {
 func (k Client) Watch(ctx context.Context, labelMap map[string]string, fn func(collectors []string)) {
 	collectors := []string{}
 
-	ns := os.Getenv("OTEL_NAMESPACE")
 	opts := metav1.ListOptions{
 		LabelSelector: labels.SelectorFromSet(labelMap).String(),
 	}
@@ -49,37 +57,42 @@ func (k Client) Watch(ctx context.Context, labelMap map[string]string, fn func(c
 	}
 	fn(collectors)
 
-	watcher, err := k.k8sClient.CoreV1().Pods(ns).Watch(ctx, opts)
-	if err != nil {
-		log.Fatal(err)
-	}
+	for {
+		watcher, err := k.k8sClient.CoreV1().Pods(ns).Watch(ctx, opts)
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	go func() {
-		for {
-			select {
-			case <-ctx.Done():
-				return
-			default:
-				for event := range watcher.ResultChan() {
-					pod, ok := event.Object.(*v1.Pod)
-					if !ok {
-						log.Fatal(err)
-					}
+		go func() {
+			for {
+				select {
+				case <-ctx.Done():
+					return
+				case <-time.After(watcherTimeout):
+					return
+				default:
+					c := watcher.ResultChan()
+					for event := range c {
+						pod, ok := event.Object.(*v1.Pod)
+						if !ok {
+							log.Fatal(err)
+						}
 
-					switch event.Type {
-					case watch.Added:
-						collectors = append(collectors, pod.Name)
+						switch event.Type {
+						case watch.Added:
+							collectors = append(collectors, pod.Name)
 
-					case watch.Deleted:
-						for i, _ := range collectors {
-							if collectors[i] == pod.Name {
-								collectors = append(collectors[:i], collectors[i+1:]...)
+						case watch.Deleted:
+							for i := range collectors {
+								if collectors[i] == pod.Name {
+									collectors = append(collectors[:i], collectors[i+1:]...)
+								}
 							}
 						}
+						fn(collectors)
 					}
-					fn(collectors)
 				}
 			}
-		}
-	}()
+		}()
+	}
 }
